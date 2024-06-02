c231ab33e60cc9749b4df653878ca74a114533ac
zzhu4308 <zzhu35@buffalo.edu>
2024-06-02 19:18:52 -0400

    simple setup still(working on css rn)

--- tankio_app/main.js
+++ tankio_app/main.js
@@ -1,1 +1,11 @@
 import './style.css'
+import Phaser from 'phaser'
+
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+  ratio: 9/16
+};
+
+
+const game = new Phaser.Game(sizes)
